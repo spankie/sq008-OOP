593a79fe58be39a1b91f56cd3b3dc740deddd1f6
Odohi David <odohi.david@gmail.com>
2021-06-30 12:24:49 +0100

    modify business logic

--- src/main/java/com/decagon/App.java
+++ src/main/java/com/decagon/App.java
@@ -5,6 +5,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.decagon.notifications.EmailSender;
+import com.decagon.notifications.impl.MailgunEmailSender;
+
 public class App {
 	public static void main(String[] args) {
 		List<Staff> staffList = new ArrayList<>();
@@ -17,18 +20,20 @@ public class App {
 		positionSalary.put("Software Engineer", 20000);
 		String name = "Hope";
 		Applicant hope = new Applicant(name, 1, "Edo Tech Park", "hope@decagon.dev", "Software Engineer", 3, "BSC");
+		Applicant musa = new Applicant("Musa", 2, "Edo Tech Park", "musa@decagon.dev", "Cleaner", 10, "SSCE");
 		// emailSender.sendHtmlEmail("odohi.david@gmail.com", "spankie@gmail.com", "Hello David", "Hi David, I'm trying to check up on you. how is the edo state?\n");
 		Ceo goodluck = new Ceo("Goodluck", 1, "Edo Tech Park, Room 119", "goodluck@gej.com", 30000, emailSender, positionSalary);
 
-		System.out.printf("Number of staff BEFORE hiring: %d\n", staffList.size());
-		goodluck.hire(hope, staffList);
-		System.out.printf("Number of staff AFTER hiring: %d\n", staffList.size());
+		// System.out.printf("Number of staff BEFORE hiring: %d\n", staffList.size());
+		// goodluck.hire(hope, staffList);
+		// System.out.printf("Number of staff AFTER hiring: %d\n", staffList.size());
 
 		Company squadEigthLLC = new Company(goodluck, staffList, paul);
 
     squadEigthLLC.apply(hope);
-		System.out.println(squadEigthLLC.getApplicants().toString());
+		squadEigthLLC.apply(musa);
+		// System.out.println(squadEigthLLC.getApplicants().toString());
 		squadEigthLLC.hire();
-    System.out.println("Hello World!");
+    System.out.println(staffList);
   }
 }
